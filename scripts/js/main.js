1cc377e3a236455727e02a81fc8a4829b56c2114
interglobalmedia <interglobalmedia@gmail.com>
2020-03-04 23:58:16 -0500

    Add files first commit

---
+++ scripts/js/main.js
@@ -0,0 +1,20 @@
+let SlideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.querySelectorAll("image-slider-fade");
+  let dots = document.querySelectorAll("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000);
+}
